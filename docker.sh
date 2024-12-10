1f2da7670d6c71c7b7ee07bcc56bb2518c2e7b85
Bruno Henrique <brunohenriquesantos272@gmail.com>
2024-12-10 09:38:27 -0300

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,19 @@
+# Adicionar a chave GPG oficial do Docker
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Adicionar o repositório do Docker à lista de fontes de pacotes do APT
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Instalar os pacotes do Docker
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Dar permissão ao usuário atual para rodar o Docker
+sudo usermod -aG docker $USER
